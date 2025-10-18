94dcba2419a72e58fc8bc39ff044ad63af7a1894
--global <--global>
2025-10-18 17:25:09 +0200

    Plantilla layout con menu de navegacion angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home').then(p => p.Home)
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact').then(p => p.Contact)
+  },
+  { path: '**', redirectTo: '' }
+];
